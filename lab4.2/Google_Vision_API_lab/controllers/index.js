4321cd8dd849c76fe8397d81226ea2e90936c864
kumar_0me <tszyin.chong@gmail.com>
2024-12-04 17:21:24 +0800

    Update index.js

--- lab4.2/Google_Vision_API_lab/controllers/index.js
+++ lab4.2/Google_Vision_API_lab/controllers/index.js
@@ -7,65 +7,77 @@ const knex = require("knex");
 const configOptions = require("../knexfile");
 const db = knex(configOptions);
 
-// **Note: Copy from the Google vision api document:
-// https://cloud.google.com/vision/docs/base64?hl=en#using_client_libraries
 function encodeImage(image) {
-  // get the path of the image you want to access
   const imageFilePath = path.resolve(__dirname, `../public/images/${image}`);
-
-  // read the image data
   const imageFile = fs.readFileSync(imageFilePath);
-
-  // Convert the image data to a Buffer and encode it to base64 format.
   const base64ImageStr = Buffer.from(imageFile).toString("base64");
   return base64ImageStr;
 }
 
 const listImageController = async (req, res) => {
-  // TODO: add you code here
+  try {
+    const images = await db("image").select("*"); // Fetch all images from the database
+    res.json(images);
+  } catch (error) {
+    res.status(500).send(`Error fetching images: ${error}`);
+  }
 };
 
 const getImageController = async (req, res) => {
-  // TODO: add you code here
+  const { id } = req.params; // Extract ID from request parameters
+  try {
+    const image = await db("image").where({ id }).first(); // Fetch image by ID
+    if (!image) {
+      return res.status(404).send("Image not found");
+    }
+    res.json(image);
+  } catch (error) {
+    res.status(500).send(`Error fetching image: ${error}`);
+  }
 };
 
 const createImageController = async (req, res) => {
   try {
-    // Google Vision API key is stored in env file
     const apiKey = process.env.API_KEY;
-
-    // req.file exist after upload.single("imageFile") middleware is resolved
     const imageFileName = req.file.originalname;
     const base64ImageStr = encodeImage(imageFileName);
 
-    // **Note: check out the document to see how to configure the request body of the google vision PAI
-    // https://cloud.google.com/vision/docs/reference/rest/v1/AnnotateImageRequest
-    request_body = {
+    const request_body = {
       requests: [
         {
           image: {
-            content: base64ImageStr, // this need to be base64 string
+            content: base64ImageStr,
           },
           features: [
             {
               type: "LABEL_DETECTION",
+              maxResults: 10, // Adjust max results as needed
             },
           ],
         },
       ],
     };
 
-    // TODO: add you code here
     const response = await axios.post(
       `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`,
-      request_body,{
-        headers:{
-          'Content-Type':'application/json',
-        }
+      request_body,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
       }
     );
-    // **Note: call the Google Vision API, check out the official document below for reference:
-    // https://cloud.google.com/vision/docs/request
+
+    const labels = response.data.responses[0].labelAnnotations; // Extract detected labels
+    const detectedLabels = labels.map(label => label.description).join(", "); // Join labels into a string
+
+    // Store the image info in the database
+    await db("image").insert({
+      name: imageFileName,
+      detected_label: detectedLabels,
+    });
+
+    res.status(201).json({ message: "Image processed successfully", labels: detectedLabels });
   } catch (error) {
     res.status(500).send(`Error occurs. Error: ${error}`);
   }
